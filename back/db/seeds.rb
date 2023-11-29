84dc35053e3ed64865f99a4807e516b989a970aa
Cyprien Pannier <cpannier@sweep.net>
2023-11-29 20:28:46 +0000

    posts rating

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -8,5 +8,5 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Post.upsert({ id: 1, title: 'The Essential Guide to Building Balanced Development Teams' })
-Post.upsert({ id: 2, title: 'RAFT Explained – Part 1/3: Introduction to the Consensus Problem' })
+Post.upsert({ id: 1, title: 'The Essential Guide to Building Balanced Development Teams', rating: 5 })
+Post.upsert({ id: 2, title: 'RAFT Explained – Part 1/3: Introduction to the Consensus Problem', rating: 4 })
